1db0b2b5c152b8d4dadd3cb503ed66270b52a237
Colin <colin.diesh@gmail.com>
2023-01-05 11:12:13 -0700

    Save track data
    Convert to AbstractTrackModel
    
    Fix strand for gff3 save track data (#3688)

--- plugins/dotplot-view/src/DotplotView/model.ts
+++ plugins/dotplot-view/src/DotplotView/model.ts
@@ -35,7 +35,7 @@ import FolderOpenIcon from '@mui/icons-material/FolderOpen'
 import PhotoCameraIcon from '@mui/icons-material/PhotoCamera'
 
 // locals
-import { Dotplot1DView, DotplotHView, DotplotVView } from './1dview'
+import { Dotplot1DView, DotplotHView, DotplotVView } from '../1dview'
 import { getBlockLabelKeysToHide, makeTicks } from './components/util'
 import { BaseBlock } from './blockTypes'
 
@@ -727,5 +727,4 @@ export default function stateModelFactory(pm: PluginManager) {
 
 export type DotplotViewStateModel = ReturnType<typeof stateModelFactory>
 export type DotplotViewModel = Instance<DotplotViewStateModel>
-
-export { type Dotplot1DViewModel, Dotplot1DView } from './1dview'
+export { type Dotplot1DViewModel, Dotplot1DView } from '../1dview'
