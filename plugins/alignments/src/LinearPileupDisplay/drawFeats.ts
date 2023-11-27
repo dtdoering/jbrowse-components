40acd0fded7d700c32f4a3dd3938514efae1e8ed
Colin <colin.diesh@gmail.com>
2023-11-26 22:14:49 -0500

    [skip ci] Misc

--- plugins/alignments/src/LinearPileupDisplay/drawFeats.ts
+++ plugins/alignments/src/LinearPileupDisplay/drawFeats.ts
@@ -2,8 +2,8 @@ import { Feature, getContainingView, getSession } from '@jbrowse/core/util'
 import { LinearGenomeViewModel } from '@jbrowse/plugin-linear-genome-view'
 
 import { LinearPileupDisplayModel } from './model'
-import { fillRectCtx, strokeRectCtx } from '../LinearReadCloudDisplay/util'
-import { fillColor, strokeColor } from '../shared/color'
+import { renderAlignment } from '../PileupRenderer/renderAlignment'
+import { getCharWidthHeight } from '../PileupRenderer/util'
 
 type LGV = LinearGenomeViewModel
 
@@ -13,23 +13,31 @@ export function drawFeats(
   width: number,
   height: number,
 ) {
-  const { featureData: features } = self
+  const { colorBy, colorTagMap, featureData: features } = self
   if (!features) {
     return
   }
   const view = getContainingView(self) as LGV
+  const { assemblyNames, bpPerPx } = view
   const { assemblyManager } = getSession(self)
-  const asm = assemblyManager.get(view.assemblyNames[0])
+  const asm = assemblyManager.get(assemblyNames[0])
   if (!asm || !self.featureData) {
     return
   }
 
-  for (const [key, val] of self.layout.getRectangles().entries()) {
-    const [l, top, r, bottom] = val
-    const w = r - l - 1
-    const featureHeight = bottom - top
-
-    strokeRectCtx(l - view.offsetPx, top, w, featureHeight - 1, ctx, 'grey')
-    fillRectCtx(l - view.offsetPx, top, w, featureHeight - 1, ctx, 'grey')
+  const { charWidth, charHeight } = getCharWidthHeight()
+  for (const [id, rect] of self.layout.rectangles.entries()) {
+    renderAlignment({
+      ctx,
+      feat: rect.data as Feature,
+      charWidth,
+      charHeight,
+      width,
+      bpPerPx,
+      colorBy,
+      colorTagMap,
+    })
+    // strokeRectCtx(l - view.offsetPx, top, w, featureHeight - 1, ctx, 'grey')
+    // fillRectCtx(l - view.offsetPx, top, w, featureHeight - 1, ctx, 'grey')
   }
 }
